12bac0ff58bbf873b6a21137f26db7280bd196d9
uguryagmur <cuy.official@gmail.com>
2022-01-12 08:49:41 +0000

    DCGAN implementation with CIFAR100

--- examples/GAN.py
+++ examples/GAN.py
@@ -1,16 +1,20 @@
 import glob
-from numpy import generic
-import tqdm
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
+import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-
+from torchvision.transforms.transforms import ToTensor
+import tqdm
 from PIL import Image
-from typing import Tuple, List
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
+
+
+torch.manual_seed(0)
 
 
 class FFHQDataset(Dataset):
@@ -32,6 +36,18 @@ class FFHQDataset(Dataset):
     def get_loader(self, batch_size: int, workers: int, shuffle: bool = True):
         return DataLoader(
             self, batch_size=batch_size, num_workers=workers, shuffle=shuffle
+        )
+
+
+class CIFAR100Dataset:
+    def __init__(self) -> None:
+        self.dataset = datasets.CIFAR100(
+            root="~/Datasets/torchvision", download=True, transform=ToTensor()
+        )
+
+    def get_loader(self, batch_size: int, workers: int, shuffle: bool = True):
+        return DataLoader(
+            self.dataset, batch_size=batch_size, num_workers=workers, shuffle=shuffle,
         )
 
 
@@ -61,33 +77,11 @@ class Generator(nn.Module):
         return nn.Sequential(
             # input size (batch_size, latent_space_size, 1, 1)
             nn.ConvTranspose2d(
-                self.latent_space_size, self.feature_map_size * 16, 4, 1, 0, bias=False
-            ),
-            nn.BatchNorm2d(self.feature_map_size * 16),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 16, 4, 4)
-            nn.ConvTranspose2d(
-                self.feature_map_size * 16,
-                self.feature_map_size * 8,
-                4,
-                2,
-                1,
-                bias=False,
-            ),
-            nn.BatchNorm2d(self.feature_map_size * 8),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 8, 8, 8)
-            nn.ConvTranspose2d(
-                self.feature_map_size * 8,
-                self.feature_map_size * 4,
-                4,
-                2,
-                1,
-                bias=False,
+                self.latent_space_size, self.feature_map_size * 4, 4, 1, 0, bias=False
             ),
             nn.BatchNorm2d(self.feature_map_size * 4),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 4, 16, 16)
+            nn.LeakyReLU(negative_slope=0.02),
+            # state size (batch_size, feature_map_size * 16, 4, 4)
             nn.ConvTranspose2d(
                 self.feature_map_size * 4,
                 self.feature_map_size * 2,
@@ -97,19 +91,19 @@ class Generator(nn.Module):
                 bias=False,
             ),
             nn.BatchNorm2d(self.feature_map_size * 2),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 2, 32, 32)
+            nn.LeakyReLU(negative_slope=0.02),
+            # state size (batch_size, feature_map_size * 8, 8, 8)
             nn.ConvTranspose2d(
-                self.feature_map_size * 2, self.feature_map_size, 4, 2, 1, bias=False
+                self.feature_map_size * 2, self.feature_map_size, 4, 2, 1, bias=False,
             ),
             nn.BatchNorm2d(self.feature_map_size),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size, 64, 64)
+            nn.LeakyReLU(negative_slope=0.02),
+            # state size (batch_size, feature_map_size * 4, 16, 16)
             nn.ConvTranspose2d(
-                self.feature_map_size, self.color_channel_size, 4, 2, 1, bias=False
+                self.feature_map_size, self.color_channel_size, 4, 2, 1, bias=False,
             ),
-            nn.Tanh(),
-            # output size (batch_size, color_channel_size, 128, 128)
+            # state size (batch_size, feature_map_size * 2, 32, 32)
+            nn.Sigmoid(),
         )
 
 
@@ -136,19 +130,20 @@ class Discriminator(nn.Module):
 
     def configure_network(self):
         return nn.Sequential(
-            # input size (batch_size, color_channel_size, 128, 128)
+            # input size (batch_size, color_channel_size, 32, 32)
             nn.Conv2d(
                 self.color_channel_size, self.feature_map_size, 4, 2, 1, bias=False
             ),
             nn.BatchNorm2d(self.feature_map_size),
             nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size, 64, 64)
+            # state size (batch_size, feature_map_size, 16, 16)
             nn.Conv2d(
                 self.feature_map_size, self.feature_map_size * 2, 4, 2, 1, bias=False,
             ),
             nn.BatchNorm2d(self.feature_map_size * 2),
             nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 2, 32, 32)
+            nn.Dropout(0.1),
+            # state size (batch_size, feature_map_size * 2, 8, 8)
             nn.Conv2d(
                 self.feature_map_size * 2,
                 self.feature_map_size * 4,
@@ -159,30 +154,9 @@ class Discriminator(nn.Module):
             ),
             nn.BatchNorm2d(self.feature_map_size * 4),
             nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 4, 16, 16)
-            nn.Conv2d(
-                self.feature_map_size * 4,
-                self.feature_map_size * 8,
-                4,
-                2,
-                1,
-                bias=False,
-            ),
-            nn.BatchNorm2d(self.feature_map_size * 8),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 8, 8, 8)
-            nn.Conv2d(
-                self.feature_map_size * 8,
-                self.feature_map_size * 16,
-                4,
-                2,
-                1,
-                bias=False,
-            ),
-            nn.BatchNorm2d(self.feature_map_size * 16),
-            nn.LeakyReLU(),
-            # state size (batch_size, feature_map_size * 16, 4, 4)
-            nn.Conv2d(self.feature_map_size * 16, 1, 4, 1, 0, bias=False),
+            nn.Dropout(0.1),
+            # state size (batch_size, feature_map_size * 4, 4, 4)
+            nn.Conv2d(self.feature_map_size * 4, 1, 4, 1, 0, bias=False),
             nn.Sigmoid(),
             # output size (batch_size, 1, 1, 1)
         )
@@ -201,7 +175,7 @@ class GANTrainer:
     writer = SummaryWriter("/home/adm1n/Shop/logs")
 
     def __init__(self):
-        self.dataset = FFHQDataset(self.dataset_path)
+        self.dataset = CIFAR100Dataset()
 
     def train(self, gen: Generator, disc: Discriminator, num_epochs: int = 100):
         data_loader = self.dataset.get_loader(
@@ -222,37 +196,37 @@ class GANTrainer:
             total_gen_loss = 0.0
             batch_progress_bar = tqdm.tqdm(data_loader)
             for batch_iter, data in enumerate(batch_progress_bar):
+                data = data[0]
+                disc_optimizer.zero_grad()
+                gen_optimizer.zero_grad()
+                noise = torch.rand(size=(self.batch_size, gen.latent_space_size, 1, 1))
+                with torch.no_grad():
+                    generated: torch.Tensor = gen(noise)
+
                 target_real = [self.real_label] * data.size(0)
                 target_fake = [self.fake_label] * self.batch_size
-                target = target_real + target_fake
-                target = torch.FloatTensor(target)
 
-                disc_optimizer.zero_grad()
-                noise = torch.rand(size=(self.batch_size, gen.latent_space_size, 1, 1))
-                generated: torch.Tensor = gen(noise)
-                disc_input = torch.cat((data.to(gen.device), generated))
-                rand_perm = torch.randperm(self.batch_size + data.size(0))
-                disc_output = disc(disc_input[rand_perm].detach()).view(-1)
+                disc_fake = disc(generated.to(disc.device)).view(-1)
+                target = torch.FloatTensor(target_fake).to(disc.device)
+                disc_fake_loss = criterion(disc_fake, target)
+                disc_fake_loss.backward(disc_fake_loss)
 
-                disc_loss: torch.Tensor = criterion(
-                    disc_output, target[rand_perm].to(disc.device)
-                )
-                disc_loss.backward(disc_loss)
+                disc_real = disc(data.to(disc.device)).view(-1)
+                target = torch.FloatTensor(target_real).to(disc.device)
+                disc_real_loss = criterion(disc_real, target)
+                disc_real_loss.backward(disc_real_loss)
                 disc_optimizer.step()
 
-                gen_optimizer.zero_grad()
                 generated: torch.Tensor = gen(noise)
-                disc_output = disc(generated).view(-1)
-                gen_loss: torch.Tensor = criterion(
-                    disc_output,
-                    torch.FloatTensor([self.real_label] * self.batch_size).to(
-                        disc.device
-                    ),
-                )
+                disc_fake = disc(generated.to(disc.device)).view(-1)
+                target = torch.FloatTensor([self.real_label] * self.batch_size)
+                gen_loss = criterion(disc_fake, target.to(disc.device))
                 gen_loss.backward(gen_loss)
                 gen_optimizer.step()
 
-                total_disc_loss += disc_loss.mean().item()
+                total_disc_loss += (
+                    disc_fake_loss.mean().item() + disc_real_loss.mean().item()
+                )
                 total_gen_loss += gen_loss.mean().item()
                 batch_progress_bar.set_postfix(
                     {
@@ -295,8 +269,9 @@ class GANTrainer:
         for elem in loss_history:
             loss_hist_d.append(elem[0])
             loss_hist_g.append(elem[1])
-        plt.plot(loss_hist_d)
-        plt.plot(loss_hist_g)
+        plt.plot(loss_hist_d, label="disc")
+        plt.plot(loss_hist_g, label="gen")
+        plt.legend()
         plt.savefig("GAN_loss_graph.png")
 
         # checkpoint for continuing to the training
@@ -311,8 +286,8 @@ def demo_generator(gen: Generator):
         while True:
             noise = torch.rand(size=(1, gen.latent_space_size, 1, 1))
             fake: torch.Tensor = gen(noise).cpu().squeeze(dim=0)
-            fake = fake.transpose(0, 1).transpose(1, 2).numpy()
-            plt.imshow(fake * 255)
+            fake = (fake * 255).transpose(0, 1).transpose(1, 2).long().numpy()
+            plt.imshow(fake)
             plt.show()
             breakpoint()
 
@@ -321,7 +296,7 @@ def main():
     generator = Generator()
     discriminator = Discriminator()
     trainer = GANTrainer()
-    trainer.train(generator, discriminator, 80)
+    trainer.train(generator, discriminator, 60)
     demo_generator(generator)
 
 
